310df43f47ca38d9862112a563fdde703cf790d5
haryssd <harisdanielnoh@gmail.com>
2025-08-08 11:06:20 +0800

    modify tagline

--- components/Tagline.tsx
+++ components/Tagline.tsx
@@ -1,8 +1,8 @@
 function Tagline() {
   return (
-    <div className="group p-[2px] bg-gradient-to-r from-blue-400 via-gray-500 to-blue-400 bg-[length:200%_100%] animate-gradient rounded-xl transition-all duration-300 hover:scale-[1.01] h-full">
-      <div className="bg-[#f7f7f7] hover:bg-[#eeeeee] rounded-[10px] shadow-sm hover:shadow-md transition-all duration-300 h-full overflow-hidden relative flex items-center justify-center">
-        <h1 className="text-2xl font-bold text-gray-900 text-center tracking-tight group-hover:text-blue-900 transition-colors duration-300">
+    <div className="group p-[2px] bg-gradient-to-r from-blue-400 via-gray-400 to-blue-400 bg-[length:200%_100%] animate-gradient rounded-xl transition-all duration-300 hover:scale-[1.01] h-full">
+      <div className="  rounded-[10px] shadow-sm hover:shadow-md transition-all duration-300 h-full overflow-hidden relative flex items-center justify-center">
+        <h1 className="text-2xl font-bold text-[#eeeeee] text-center tracking-tight group-hover:text-gray-900 transition-colors duration-300">
           Code. Create. Innovate.
         </h1>
       </div>
